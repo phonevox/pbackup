48e94baacb54072557de6ea47ef3aad7e65bf041
Adrian <adriankubinyete@outlook.com>
2025-03-19 18:11:42 -0300

    fix: script/issabel.sh using wrong logs

--- scripts/issabel.sh
+++ scripts/issabel.sh
@@ -41,25 +41,7 @@ parse_flags $@
 
 # === FUNCS ===
 
-function log () {
-    local CURRTIME=$(date '+%Y-%m-%d %H:%M:%S')
-    if ! [ -f "$LOG_FILE" ]; then
-        echo -e "[$CURRTIME] $SCRIPT_NAME> Iniciando novo logfile" > "$LOG_FILE"
-    fi
-
-    if [ -z $2 ]; then
-        local muted=false
-    else 
-        local muted=true
-    fi
-
-    echo -e "[$CURRTIME] $SCRIPT_NAME> $1" >> "$LOG_FILE"
-    if ! $muted; then
-        echo -e "[$CURRTIME] $SCRIPT_NAME> $1"
-    fi
-}
-
-log "=== STARTING - ARGUMENTS: $*" muted
+log.debug "=== STARTING - ARGUMENTS: $*" muted
 
 function generate_backup_file() {
 
@@ -67,24 +49,24 @@ function generate_backup_file() {
 
     # adding extra components if our custom bkp engine is installed
     if [ -f "/usr/share/issabel/privileged/pvx-backupengine-extras" ]; then
-        log "Custom backup engine detected, adding extra components to backup..."
+        log.debug "Custom backup engine detected, adding extra components to backup..."
         CUSTOM_BACKUPENGINE_COMPONENTS=",int_ixcsoft,int_sgp,int_receitanet,int_altarede" # start with comma!!!!!!
     else
-        log "Custom backup engine not detected, skipping extra components..."
+        log.debug "Custom backup engine not detected, skipping extra components..."
     fi
     COMPONENTS="$COMPONENTS$CUSTOM_BACKUPENGINE_COMPONENTS"
-    log "Components to backup: $COMPONENTS"
+    log.trace "Components to backup: $COMPONENTS"
 
-    log "Generating Issabel backup, this might take a while..."
+    log.info "Generating Issabel backup, this might take a while..."
     /usr/bin/issabel-helper backupengine --backup --backupfile "$BACKUP_FILE" --tmpdir "$BACKUP_DIR" --components $COMPONENTS 2>&1
 
     # checking if backup was generated
     if ! [ -f "$BACKUP_DIR/$BACKUP_FILE" ]; then
-        log "ERROR: '$BACKUP_DIR/$BACKUP_FILE' was not generated! Exiting for safety reasons..."
+        log.fatal "ERROR: '$BACKUP_DIR/$BACKUP_FILE' was not generated! Exiting for safety reasons..."
         exit 1
     fi
 
-    log "'$BACKUP_DIR/$BACKUP_FILE' generated, proceeding..."
+    log.info "'$BACKUP_DIR/$BACKUP_FILE' generated, proceeding..."
 }
 
 # === RUNTIME ===
@@ -93,43 +75,43 @@ function validations () {
     FULL_REMOTE_DEST="$(getFlag "t")"
 
     # if not first argument, quit 
-    log "Checking for required arguments..."
+    log.trace "Checking for required arguments..."
     if  [ -z "$FULL_REMOTE_DEST" ]; then
-        log "ERROR: You need to provide an rclone remote destination! Example: -t \"mega:/backup\""
+        log.fatal "ERROR: You need to provide an rclone remote destination! Example: -t \"mega:/backup\""
         exit 1
     fi
 
     # test if usr/sbin/pbackup exists
-    log "Checking if pbackup is installed..."
+    log.trace "Checking if pbackup is installed..."
     if ! [ -f "/usr/sbin/pbackup" ]; then
-        log "ERROR: You need to install pbackup! Exiting..."
+        log.fatal "ERROR: You need to install pbackup! Exiting..."
         exit 1
     fi
 
     # check if remote exists
-    log "Checking if remote exists..."
+    log.trace "Checking if remote exists..."
     REMOTE_NAME="$(echo $FULL_REMOTE_DEST | cut -d ':' -f 1)"
     if ! pbackup --list | grep -q "^$REMOTE_NAME:"; then
-        log "ERROR: Remote $REMOTE_NAME not found! Exiting..."
+        log.fatal "ERROR: Remote $REMOTE_NAME not found! Exiting..."
         exit 1
     fi
 
     # checking for issabelhelper, to generate the backups. if this is not present this may not be an issabel server
-    log "Checking for issabel-helper to handle backup generation..."
+    log.trace "Checking for issabel-helper to handle backup generation..."
     if ! [ -f "/usr/bin/issabel-helper" ]; then
-        log "ERROR: '/usr/bin/issabel-helper' not found. Is this really an IssabelPBX?"
+        log.fatal "ERROR: '/usr/bin/issabel-helper' not found. Is this really an IssabelPBX?"
         exit 1
     fi
 }
 
 function execute_rotate () {
-    log "Rotating via scanline/pinpoint for $ROTATE days..."
+    log.trace "Rotating via scanline/pinpoint for $ROTATE days..."
     hasFlag "rec" && rotate_recordings
     hasFlag "config" && rotate_configuration
-    log "The rotated files are: $(echo ${ROTATED_FILES[@]})"
+    log.trace "The rotated files are: $(echo ${ROTATED_FILES[@]})"
 
     for ROTATED_FILE in "${ROTATED_FILES[@]}"; do
-        log "Removing $ROTATED_FILE"
+        log.trace "Removing $ROTATED_FILE"
         pbackup --delete "$ROTATED_FILE"
 
         # TODO(adrian): purge empty folders. only really needed for recordings
@@ -137,7 +119,7 @@ function execute_rotate () {
 }
 
 function rotate_recordings () {
-    log "Rotating recordings..."
+    log.info "Rotating recordings..."
     local REMOTE="$FULL_REMOTE_DEST$_REMOTE_FOLDER_RECORDINGS"
 
     # i dont know why you would rotate recordings, but okay, ill make it somewhat compatible
@@ -150,28 +132,28 @@ function rotate_recordings () {
     local DAY=$(date -d "$ROTATE days ago" +%d)
     local HIT="$REMOTE/$YEAR/$MONTH/$DAY"
 
-    log "rotate_recordings: SCANLINE TARGET: remote_folder '$HIT' "
+    log.trace "rotate_recordings: SCANLINE TARGET: remote_folder '$HIT' "
     if rclone lsf "$HIT" > /dev/null; then # if the command fails, the folder does not exist
-        log "rotate_recordings: scanline hit: $HIT"
+        log.trace "rotate_recordings: scanline hit: $HIT"
         ROTATED_FILES+=("$HIT")
     else
-        log "rotate_recordings: scanline miss: $HIT"
+        log.trace "rotate_recordings: scanline miss: $HIT"
     fi
 }
 
 function rotate_configuration () {
-    log "Rotating configuration..."
+    log.info "Rotating configuration..."
     local REMOTE="$FULL_REMOTE_DEST$_REMOTE_FOLDER_CONFIGURATION"
     local TARGET="issabelbackup"
     local TARGET_DATE=$(date -d "$ROTATE days ago" +%Y-%m-%d)
 
-    log "rotate_configuration: SCANLINE TARGET: date '$TARGET_DATE' | target '$TARGET' | remote '$REMOTE' "
+    log.trace "rotate_configuration: SCANLINE TARGET: date '$TARGET_DATE' | target '$TARGET' | remote '$REMOTE' "
     while read -r __SIZE __DATE __TIME __FILE; do
         if [[ "$__DATE" == "$TARGET_DATE" ]] && [[ "$__FILE" == *"$TARGET"* ]]; then
-            log "rotate_configuration: scanline hit: $__SIZE $__DATE $__TIME $__FILE"
+            log.trace "rotate_configuration: scanline hit: $__SIZE $__DATE $__TIME $__FILE"
             ROTATED_FILES+=("$REMOTE/$__FILE")
         else
-            log "rotate_configuration: scanline miss: $__SIZE $__DATE $__TIME $__FILE"
+            log.trace "rotate_configuration: scanline miss: $__SIZE $__DATE $__TIME $__FILE"
         fi
     done < <(rclone lsl "$REMOTE")
 }
@@ -201,23 +183,23 @@ function main () {
     fi
 
     # ======== UPLOAD ========
-    log "Uploading through pbackup..."
+    log.debug "Uploading through pbackup..."
     FILES=$(echo "${FILES[@]}" | tr ' ' ',')
     pbackup --files "$FILES" --to "$FULL_REMOTE_DEST"
 
     # ======== CLEAN UP ========
-    log "Cleaning backupfile from local machine..."
+    log.debug "Cleaning backupfile from local machine..."
     if [ -f "$BACKUP_DIR/$BACKUP_FILE" ]; then
         rm -f "$BACKUP_DIR/$BACKUP_FILE"
-        log "- '$BACKUP_DIR/$BACKUP_FILE' deleted."
+        log.trace "- '$BACKUP_DIR/$BACKUP_FILE' deleted."
     else
-        log "ERROR: '$BACKUP_DIR/$BACKUP_FILE' was not found. We aren't going to perform any delete operation in order to avoid deleting other files. Location checked: \"$BACKUP_DIR/$BACKUP_FILE\""
+        log.error "ERROR: '$BACKUP_DIR/$BACKUP_FILE' was not found. We aren't going to perform any delete operation in order to avoid deleting other files. Location checked: \"$BACKUP_DIR/$BACKUP_FILE\""
     fi
 
     # ======== ROTATE ========
     [[ "$ROTATE" != "false" ]] && execute_rotate
 
-    log "All done!"
+    log.info "All done!"
     exit 0
 }
 
